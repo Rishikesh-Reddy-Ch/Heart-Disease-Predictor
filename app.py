6a363608312cc05f442738e7866921aa02d956e8
kenshi-0 <rishikesh.cheruku22@kmit.edu.in>
2023-11-09 14:07:25 +0530

    Update-2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request,flash,redirect,url_for,session,get_flashed_messages
 import functions as fn
+import pandas as pd
 # import jsonify
 
 app = Flask(__name__)
@@ -25,33 +26,18 @@ def register():
       return redirect(url_for('register'))
       
     elif  fn.numCheck(phoneNum) and  fn.emailValidate(email):
-      fn.updateCredentials(username,password,phoneNum,email)
+      updated=fn.updateCredentials(username,password,phoneNum,email)
+      if not updated:
+         flash("Unable to connect to database","error")
+         return redirect(url_for('register'))
       return redirect(url_for('login'))
     
     else:
       flash("Invalid Phone Number or emailId",'error')
 
       return redirect(url_for('register'))
-  return render_template('register.html')
+  return render_template('register.html',messages=get_flashed_messages(with_categories=True))
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == "GET":
-#         username = request.args.get('username')
-#         password = request.args.get('password')
-
-        
-#         user = fn.verify_credentials(username, password)
-        
-#         if user:
-#             # session['user'] = username
-#             return redirect(url_for('index'))
-
-#         else:
-#             flash('Invalid credentials, please try again.', 'error')
-    
-#     messages = get_flashed_messages();
-#     return render_template('login.html',messages=messages)  
   
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -62,13 +48,24 @@ def login():
         user = fn.verify_credentials(username, password)
 
         if user:
-            return redirect(url_for('index'))
+            session["Username"]=username
+            return redirect(url_for('form'))
         else:
             flash('Invalid credentials, please try again.', 'error')
             return(redirect(url_for('login')))
 
     messages = get_flashed_messages()
     return render_template('login.html', messages=messages)
-
+@app.route('/form',methods=['GET','POST'])
+def form():
+  if request.method=='POST':
+    names=['HadHeartAttack','HighBloodPressure','AnyHeartStroke','KidneyDisease','Diabetis','DiabetesAge','smoking','exercise','HighCholLevel','Gender','DateOfBirth','Height','Weight','Drinker']
+    formdata={}
+    for i in names:
+      formdata[i]=request.form[i]
+    if not fn.process_data(formdata,names,session["Username"]):
+       flash("Unable to access the dataBase","error")
+       return redirect(url_for("form"))
+  return render_template("form.html")
 if __name__ == "__main__":
   app.run(debug=True)
